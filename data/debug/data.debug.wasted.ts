3c81bec854955ae721b8a807e86639a5054ce7c0
JumpSpinn <jumpspinn@gmx.de>
2025-10-23 14:19:33 +0200

    remove data folder

--- data/debug/data.debug.wasted.ts
+++
@@ -1,8 +0,0 @@
-import { IWasted } from "@/interfaces/Character/IWasted.ts";
-
-export class DebugData_Wasted {
-	public static readonly wastedDeath : IWasted = {
-		remainingTime: 15 * 60 * 1_000,
-		permDeath: false
-	};
-}
